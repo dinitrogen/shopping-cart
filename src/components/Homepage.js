8833cab0e7866a0a5bc39dd42dcd66a29ff386d9
dinitrogen <noshowdon@gmail.com>
2021-08-30 08:51:36 -0400

    logo testing

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import sawIcon from '../img/saw.svg';
 
 const Homepage = () => {
   return (
       <div>
         <h1>Hand-crafted homes and modular accessories for guinea pigs, hamsters, and other small pets </h1>
+
+        <div className="logoContainer">
+            <img className="logoImg" src={sawIcon} alt="saw" />
+        </div>
       </div>
   );
 }
