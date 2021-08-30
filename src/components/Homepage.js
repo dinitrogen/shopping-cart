7f3fb48a7ec5ac49b4d1cb203457655e96b25f01
dinitrogen <noshowdon@gmail.com>
2021-08-30 09:23:06 -0400

    adjustments to cart icon and qty

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,10 +5,7 @@ const Homepage = () => {
   return (
       <div>
         <h1>Hand-crafted homes and modular accessories for guinea pigs, hamsters, and other small pets </h1>
-        <h2>logo testing (nav bar edit)</h2>
-        <div className="logoContainer">
-            <img className="logoImg" src={sawIcon} alt="saw" />
-        </div>
+        
       </div>
   );
 }
